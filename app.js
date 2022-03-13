cf3e9a09f5c222019d29d23582f31dbbe9c4c91d
taiphan2288 <67281521+taiphan2288@users.noreply.github.com>
2022-03-13 15:52:20 +0700

    fix

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const navSlide = () => {
+  const burger = document.querySelector(".nav__menu-bar");
+  const navMenu = document.querySelector(".nav__menu");
+  burger.addEventListener("click", () => {
+    //   toggle nav
+    navMenu.classList.toggle("nav--active");
+  });
+  //
+};
 
+navSlide();
